ec8b0428f5a430d3937e50a0a33b543d17b4daaf
PankajBarna <100672142+PankajBarna@users.noreply.github.com>
2025-09-18 12:52:57 +0000

    studied comparisions

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,17 @@
+console.log(2 > 1)  //greater than
+console.log(2 >= 1) //greater than and equal to
+console.log( 2 == 1)    //equal to
+console.log (2 != 1)    //not equal to
+
+
+// console.log(null > 0)    //avoid such kind of conversions
+// console.log(null == 0)
+// console.log(null >= 0)
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined > 0)
+
+console.log("2" == 2)
+
+console.log("2" === 2)  //strict check (checks value as well as datatype)
