0d91b12bb4ed8960e7b5e7df383e7113a70f08ef
PankajBarna <100672142+PankajBarna@users.noreply.github.com>
2025-09-18 12:20:32 +0000

    studied type conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,15 @@
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+let someString = "45"
+// console.log(typeof someString)
+let numberString = Number(someString)
+// console.log(typeof numberString)
+
+
+let value = 3
+let negativeValue = -value
+console.log(negativeValue)
