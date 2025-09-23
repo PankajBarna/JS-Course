735a75078c0856827fcf756142b1d11c8cfe5c86
PankajBarna <100672142+PankajBarna@users.noreply.github.com>
2025-09-23 14:51:11 +0000

    studied do-while and while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,21 @@
+// While Loop -------------------------------------------------
+let index = 0
+while (index <= 10) {
+    // console.log(`Value of Index is ${index}`)
+    index = index + 1
+}
+
+// with array--------------------------------------------------
+// let myHeros = ["flash", "spiderman", "hulk", "superman", "batman"]
+// let i = 0
+// while (i < myHeros.length) {
+//     console.log(`My hero: ${myHeros[i]}`)
+//     i++
+// }
+
+//Do While loop ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+let score = 0
+do {                                            //in do-while loop work is done first and condition is checked later
+    console.log(`Score is: ${score}`)       
+    score++
+} while (score <= 10);      //score is printed till it is less than or equal to 10
