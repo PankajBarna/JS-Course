d1e5d46cd6d39425b93e7c1381fea24359ba37f9
PankajBarna <100672142+PankajBarna@users.noreply.github.com>
2025-09-24 10:19:51 +0000

    studied map function & function chaining

---
+++ 05_iterations/seven.js
@@ -0,0 +1,24 @@
+// Map function +++++++++++++++++++++++++++++++++++++
+const nums = [1,2,3,4,5,6,7,8,9]
+const newNums = nums.map((num) => (num + 10))
+// console.log(newNums)
+
+
+
+const books = [
+  { title: "The Alchemist", genre: "Fiction", publish: 1988, edition: "1st" },
+  { title: "Atomic Habits", genre: "Self-Help", publish: 2018, edition: "2nd" },
+  { title: "Sapiens", genre: "History", publish: 2011, edition: "3rd" },
+  { title: "Clean Code", genre: "Programming", publish: 2008, edition: "1st" },
+  { title: "The Psychology of Money", genre: "Finance", publish: 2020, edition: "1st" },
+  { title: "Meditations", genre: "Philosophy", publish: 1980, edition: "Revised" },
+  { title: "To Kill a Mockingbird", genre: "Classic", publish: 1960, edition: "50th Anniversary" },
+  { title: "The Pragmatic Programmer", genre: "Programming", publish: 1999, edition: "20th Anniversary" },
+  { title: "Ikigai", genre: "Self-Help", publish: 2016, edition: "1st" },
+  { title: "Thinking, Fast and Slow", genre: "Psychology", publish: 2011, edition: "2nd" }
+];
+
+// const filterBooks = books.map((book) => (book.title))
+//function chaining-----use of filter and map function combined--------------------------------------
+const filterBooks = books.filter((book) => (book.genre === "Self-Help")).map((book) => book.title)
+console.log(filterBooks)            // o/p: [ 'Atomic Habits', 'Ikigai' ]
